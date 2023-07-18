3fb3c560910bcb33c568a804860a65846c9f5ec0
unknown <hectoracosta5@gmail.com>
2023-07-18 13:41:59 -0400

    adsa

--- testnew.py
+++ testnew.py
@@ -79,8 +79,8 @@ class TVSocket(Thread):
     
     def suscribir_data(self,ws, pairs):
         print("suscribir_data", pairs)
-        auth_token = self.get_auth_token()
-        self.sendMessage(ws, "set_auth_token", [auth_token])
+    #    auth_token = self.get_auth_token()
+     #   self.sendMessage(ws, "set_auth_token", [auth_token])
         session = self.generateSession()
         self.sendMessage(ws, "quote_create_session", [session])
         self.sendMessage(ws, "quote_set_fields", [session, 'base-currency-logoid', 'ch', 'chp', 'currency-logoid', 'currency_code', 'current_session', 'description', 'exchange', 'format', 'fractional', 'is_tradable', 'language', 'local_description', 'logoid', 'lp', 'lp_time', 'minmov', 'minmove2', 'original_name', 'pricescale', 'pro_name', 'short_name', 'type', 'update_mode', 'volume', 'ask', 'bid', 'fundamentals', 'high_price', 'low_price', 'open_price', 'prev_close_price', 'rch', 'rchp', 'rtc', 'rtc_time', 'status', 'industry', 'basic_eps_net_income', 'beta_1_year', 'market_cap_basic', 'earnings_per_share_basic_ttm', 'price_earnings_ttm', 'sector', 'dividends_yield', 'timezone', 'country_code', 'provider_id']) 
